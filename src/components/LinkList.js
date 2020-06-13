f4c8aa30a6215f06d87f3845514b4c41be50e9a4
Ward <wchamberlain89@gmail.com>
2020-06-12 18:10:17 -0700

    Complete Tutorial

---
+++ src/components/LinkList.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react'
+
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
+import Link from './Link'
+
+export const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`
+
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+    }
+    votes {
+      id
+      user {
+        id
+      }
+    }
+  }
+`
+
+class LinkList extends Component {
+  
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY })
+  
+    const votedLink = data.feed.links.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+  
+    store.writeQuery({ query: FEED_QUERY, data })
+  }
+
+  _subscribeToNewLinks = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        if(!subscriptionData.data) return prev
+        const newLink = subscriptionData.data.newLink
+        const exists = prev.feed.links.find( ({ id }) => id === newLink.id);
+        if (exists) return prev;
+
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename
+          }
+        })
+      }
+    })
+  }
+
+  render() {
+    return (
+      <Query query={FEED_QUERY}>
+        { ({ loading, error, data, subscribeToMore }) => {
+          if (loading) return <div>Fetching</div>
+          if (error) return <div>Error</div>
+
+          this._subscribeToNewLinks(subscribeToMore)
+
+          const linksToRender = data.feed.links
+          return (
+            <div>
+              {linksToRender.map((link, index) => (
+                <Link 
+                  key={link.id} 
+                  link={link} 
+                  index={index} 
+                  updateStoreAfterVote={this._updateCacheAfterVote}
+                  />
+              ))}
+            </div>
+          )
+        }}
+      </Query>
+    )
+  }
+}
+
+export default LinkList
